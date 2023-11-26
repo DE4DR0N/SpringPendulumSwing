1a811a005b49e67624ac741f24a01be868958ba0
Eugene <eugene.gedrevich@outlook.com>
2023-11-27 02:52:25 +0300

    hotfix

--- src/MainPage.java
+++ src/MainPage.java
@@ -1,13 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class MainPage extends JFrame{
-    public MainPage(){
+public class MainPage extends JFrame {
+    public MainPage() {
         super("Launch Page");
         JButton buttonSpringSim = new JButton("Open application");
         JButton buttonInformAuth = new JButton("About the author");
 
+        buttonSpringSim.setFocusable(false);
         buttonSpringSim.addActionListener(e -> new SpringSim());
+        buttonInformAuth.setFocusable(false);
         buttonInformAuth.addActionListener(e -> new InformAuth());
 
         setLayout(new GridLayout(2,1));
