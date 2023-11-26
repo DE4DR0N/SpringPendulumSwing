84607e8673e872fe055d3c1c25c61fdad57a81d5
Eugene <eugene.gedrevich@outlook.com>
2023-11-27 02:52:58 +0300

    add realisation

--- src/InformAuth.java
+++ src/InformAuth.java
@@ -1,14 +1,43 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class InformAuth {
-    JFrame frame = new JFrame("About the author");
+    public InformAuth() {
+        JFrame frame = new JFrame("About the author");
 
-    InformAuth() {
+        // Установка менеджера компоновки
+        frame.setLayout(new BorderLayout());
 
+        // Добавление фотографии
+        JPanel imagePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        ImageIcon icon = new ImageIcon("src/Imgs/IMG_2003.jpg");
+        icon = new ImageIcon(icon.getImage().getScaledInstance(256,256, Image.SCALE_SMOOTH));
+        JLabel imageLabel = new JLabel(icon);
+        imagePanel.add(imageLabel);
+        frame.add(imagePanel, BorderLayout.NORTH);
 
+        // Создание панели для текста
+        JPanel textPanel = new JPanel();
+        textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
+        textPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        // Номер учебной группы
+        JLabel groupLabel = new JLabel("Группа: Ваша_Группа");
+        groupLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        textPanel.add(groupLabel);
+        // ФИО
+        JLabel nameLabel = new JLabel("ФИО: Ваши_Имя_Фамилия");
+        nameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        textPanel.add(nameLabel);
+        // Mail
+        JLabel mailLabel = new JLabel("eugene.deadron@gmail.com");
+        mailLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        textPanel.add(mailLabel);
+        frame.add(textPanel, BorderLayout.CENTER);
 
+        // Установка свойств окна
+        frame.setSize(280, 480);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(480,640);
+        frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
