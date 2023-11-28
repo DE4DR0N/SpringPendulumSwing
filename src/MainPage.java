4a5706f1453e27186a0262d1542e22328d4423e0
Eugene <eugene.gedrevich@outlook.com>
2023-11-29 01:31:05 +0300

    hotfix

--- src/MainPage.java
+++ src/MainPage.java
@@ -3,18 +3,19 @@ import java.awt.*;
 
 public class MainPage extends JFrame {
     public MainPage() {
-        super("Launch Page");
+        super("Main Window");
         JButton buttonSpringSim = new JButton("Open application");
         JButton buttonInformAuth = new JButton("About the author");
 
         buttonSpringSim.setFocusable(false);
         buttonSpringSim.addActionListener(e -> new SpringSim());
         buttonInformAuth.setFocusable(false);
-        buttonInformAuth.addActionListener(e -> new InformAuth());
+        buttonInformAuth.addActionListener(e -> new Author());
 
-        setLayout(new GridLayout(2,1));
+        setLayout(new GridLayout(3,1));
         add(buttonSpringSim);
         add(buttonInformAuth);
+
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(640,480);
@@ -22,6 +23,6 @@ public class MainPage extends JFrame {
         setVisible(true);
     }
     public static void main(String[] args) {
-        new MainPage();
+        new SplashWindow();
     }
 }
