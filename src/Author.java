95a4d8cbb1379157c64b277ab1e6a78475439be8
Eugene <eugene.gedrevich@outlook.com>
2023-12-10 18:07:31 +0300

    hotfix

--- src/Author.java
+++ src/Author.java
@@ -1,49 +1,48 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Author {
+public class Author extends JDialog {
     public Author() {
-        JFrame frame = new JFrame("About the author");
-
         // Установка менеджера компоновки
-        frame.setLayout(new BorderLayout());
+        setLayout(new BorderLayout());
         // Добавление фотографии
         JPanel imagePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         ImageIcon icon = new ImageIcon("src/pictures/IMG_2003.jpg");
         icon = new ImageIcon(icon.getImage().getScaledInstance(256,256, Image.SCALE_SMOOTH));
         JLabel imageLabel = new JLabel(icon);
         imagePanel.add(imageLabel);
-        frame.add(imagePanel, BorderLayout.NORTH);
+        add(imagePanel, BorderLayout.NORTH);
 
         // Создание панели для компонентов
-        JPanel cmpntsPanel = new JPanel();
-        cmpntsPanel.setLayout(new BoxLayout(cmpntsPanel, BoxLayout.Y_AXIS));
-        cmpntsPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        JPanel elementsPanel = new JPanel();
+        elementsPanel.setLayout(new BoxLayout(elementsPanel, BoxLayout.Y_AXIS));
+        elementsPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
         // ФИО
         JLabel nameLabel = new JLabel("ФИО: Гедревич Евгений Анатольевич");
         nameLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        cmpntsPanel.add(nameLabel);
+        elementsPanel.add(nameLabel);
         // Номер учебной группы
         JLabel groupLabel = new JLabel("Группа: 10702221");
         groupLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        cmpntsPanel.add(groupLabel);
+        elementsPanel.add(groupLabel);
         // Mail
         JLabel mailLabel = new JLabel("eugene.deadron@gmail.com");
         mailLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
-        cmpntsPanel.add(mailLabel);
-        frame.add(cmpntsPanel, BorderLayout.CENTER);
-        cmpntsPanel.add(Box.createVerticalStrut(20));
+        elementsPanel.add(mailLabel);
+        add(elementsPanel, BorderLayout.CENTER);
+        elementsPanel.add(Box.createVerticalStrut(20));
         // Button
-        JButton bttnBack = new JButton("Назад");
-        bttnBack.setAlignmentX(Component.CENTER_ALIGNMENT);
-        bttnBack.addActionListener(e -> frame.dispose());
-        cmpntsPanel.add(bttnBack);
+        JButton btnBack = new JButton("Назад");
+        btnBack.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnBack.addActionListener(e -> dispose());
+        elementsPanel.add(btnBack);
 
         // Установка свойств окна
-        frame.setSize(280, 420);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
+        setTitle("Об авторе");
+        setSize(280, 420);
+        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 }
