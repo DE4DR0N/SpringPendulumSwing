d32aa8f2db2eb8624711a9f709e1aa750bf68ad9
Eugene <eugene.gedrevich@outlook.com>
2023-12-10 18:07:40 +0300

    refactor

--- src/SpringPendulum.java
+++ src/SpringPendulum.java
@@ -1,85 +1,45 @@
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
+public class SpringPendulum {
+    private static final double MASS = 1.0;
+    private static final double SPRING_CONSTANT = 10.0;
+    private final double gravity;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+    private double displacement;
+    private double velocity;
+    private double acceleration;
+    private double time;
 
-public class SpringPendulum extends JFrame {
 
-    private static final int WIDTH = 1280;
-    private static final int HEIGHT = 720;
-    private static final double TARGET_TIME = 10.0; // Время, через которое таймер будет остановлен
-
-    private DynamicPendulum pendulum;
-
-    private XYSeries displacementSeries;
-    private XYSeries velocitySeries;
-    private XYSeries accelerationSeries;
-
-    private Timer timer;
-
-    public SpringPendulum() {
-        super("Spring Pendulum");
-
-        pendulum = new DynamicPendulum(1.0, 0.1, 0.0, 9.81);
-
-        displacementSeries = new XYSeries("Displacement");
-        velocitySeries = new XYSeries("Velocity");
-        accelerationSeries = new XYSeries("Acceleration");
-
-        setupUI();
+    public SpringPendulum(double initialDisplacement, double initialVelocity, double initialAcceleration, double gravity) {
+        this.displacement = initialDisplacement;
+        this.velocity = initialVelocity;
+        this.acceleration = initialAcceleration;
+        this.time = 0.0;
+        this.gravity = gravity;
     }
 
-    private void setupUI() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(WIDTH, HEIGHT);
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-
-        mainPanel.add(createChartPanel(displacementSeries, "Displacement"));
-        mainPanel.add(createChartPanel(velocitySeries, "Velocity"));
-        mainPanel.add(createChartPanel(accelerationSeries, "Acceleration"));
-
-        add(mainPanel);
-
-        timer = new Timer(20, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                double time = pendulum.getTime();
-                double displacement = pendulum.getDisplacement();
-                double velocity = pendulum.getVelocity();
-                double acceleration = pendulum.getAcceleration();
-
-                displacementSeries.add(time, displacement);
-                velocitySeries.add(time, velocity);
-                accelerationSeries.add(time, acceleration);
-
-                pendulum.update();
-
-                if (time >= TARGET_TIME) {
-                    timer.stop();
-                }
-            }
-        });
-        timer.start();
+    public double getDisplacement() {
+        return displacement;
     }
 
-    private JPanel createChartPanel(XYSeries series, String chartTitle) {
-        XYSeriesCollection dataset = new XYSeriesCollection(series);
-        JFreeChart chart = ChartFactory.createXYLineChart(
-                chartTitle, "Time", "Value", dataset);
-        XYPlot plot = chart.getXYPlot();
-        plot.setDomainPannable(true);
-        plot.setRangePannable(true);
+    public double getVelocity() {
+        return velocity;
+    }
 
-        return new ChartPanel(chart);
+    public double getAcceleration() {
+        return acceleration;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void update() {
+        double springForce = -SPRING_CONSTANT * displacement;
+        double netForce = MASS * gravity + springForce;
+
+        acceleration = netForce / MASS;
+        velocity += acceleration * 0.02; // time step = 0.02 seconds
+        displacement += velocity * 0.02;
+        time += 0.02;
     }
 }
-
